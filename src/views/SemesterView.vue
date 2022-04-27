5ef15c0ff6362be5fe9df71789b9f544d65ac811
Nikolai Beck <n.beck@sandata.de>
2022-04-27 15:09:43 +0200

    add table to semester

--- src/views/SemesterView.vue
+++ src/views/SemesterView.vue
@@ -1,9 +1,33 @@
 <template>Semester
  <semester-creation></semester-creation>
 
+    <LectureDate 
+    :data= "data"
+    :columns= "columns">
+  </LectureDate>
 </template>
-<script setup>
+<script>
 import { store } from "../store";
 import SemesterCreation from "../components/SemesterCreation.vue";
-
+import LectureDate from "../components/LectureDate.vue";
+export default {
+  components: {
+    LectureDate,
+    SemesterCreation
+  },
+  data() {
+    return {
+        store: store,
+      data: store.lectureDates,
+      columns: [
+        "name",
+        "startDate",
+        "endDate",
+        "lecturer",
+        "lecture",
+        "semester",
+      ],
+    };
+  },
+};
 </script>
