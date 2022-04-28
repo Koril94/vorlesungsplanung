5b8a74046fbcbc5e96445b3cebf83caf97ea41c5
Jens <j.hauner@dotsource.de>
2022-04-28 17:33:09 +0200

    changed carousel images

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -9,21 +9,35 @@
       <div class="carousel-item active" data-bs-interval="3000">
         <img
           class="d-block w-100"
-          src="../assets/images/hall-congress-architecture-building-159213.jpeg"
+          src="../assets/images/Gebaude.png"
           alt="First slide"
         />
       </div>
       <div class="carousel-item" data-bs-interval="3000">
         <img
           class="d-block w-100"
-          src="../assets/images/pexels-photo-313690.jpeg"
+          src="../assets/images/Girl_Buch.png"
           alt="Second slide"
         />
       </div>
       <div class="carousel-item" data-bs-interval="3000">
         <img
           class="d-block w-100"
-          src="../assets/images/hall-congress-architecture-building-159213.jpeg"
+          src="../assets/images/Girl_Laptop.png"
+          alt="First slide"
+        />
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img
+          class="d-block w-100"
+          src="../assets/images/Head.png"
+          alt="First slide"
+        />
+      </div>
+      <div class="carousel-item" data-bs-interval="3000">
+        <img
+          class="d-block w-100"
+          src="../assets/images/room.png"
           alt="First slide"
         />
       </div>
